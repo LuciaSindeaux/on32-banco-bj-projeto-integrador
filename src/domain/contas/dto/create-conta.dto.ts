3108bf0c587b94cba62dbcbeed0265eaeb12c094
Lucia Sindeaux <luciasindeaux@gmail.com>
2024-08-04 12:30:38 -0300

    fix: Ajustes no código para que rode, falta atualizar os repositories e services

--- src/domain/contas/dto/create-conta.dto.ts
+++
@@ -1,9 +0,0 @@
-import { TipoConta } from '../../enums/tipo-conta-enum';
-
-export class CreateContaDto {
-  clienteId: string;
-  tipoConta: TipoConta;
-  saldoInicial: number;
-  limiteChequeEspecial?: number; 
-  taxaJuros?: number; 
-}
--- src/domain/gerentes/dto/update-gerente.dto.ts
+++
@@ -1,8 +0,0 @@
-import { Cliente } from "src/domain/clientes/entities/cliente.entity";
-
-export class UpdateGerenteDto  {
-
-    nomeCompleto: string;
-    id: string;
-    clientes?: Cliente[];
-}
