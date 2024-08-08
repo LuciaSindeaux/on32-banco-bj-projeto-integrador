7e0879d42d98e882bb3608897078e0827ff104a8
Lúcia Sindeaux <107223861+LuciaSindeaux@users.noreply.github.com>
2024-08-08 20:42:04 -0300

    Revert "Entrega parcial, hexagonal, falta ajustar gerentes e contas e atualizar os testes;"

--- src/Application/dtos/clientes/update-cliente.dto.ts
+++
@@ -1,9 +0,0 @@
-import { TipoConta } from '../../../domain/enums/tipo-conta-enum';
-
-export class UpdateClienteDto {
-  nomeCompleto: string;
-  endereco?: string;
-  telefone?: string;
-  gerenteId?: string;
-  tipoConta?: TipoConta;
-}
--- src/Application/dtos/gerentes/create-gerente.dto.ts
+++
@@ -1,7 +0,0 @@
-import { Cliente } from '../../../domain/clientes/entities/cliente.entity'; 
-
-export class CreateGerenteDto {
-    nomeCompleto: string;
-    id: string;
-    clientes: Cliente[];
-}
